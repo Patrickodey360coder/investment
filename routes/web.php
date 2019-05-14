3ada94134ce4d7e3a37eba742068f01c39bf1aef
Jonathan Utsu Undelikwo <jundelikwo@gmail.com>
2019-05-14 17:59:32 +0100

    Added UserOnly middleware

--- routes/web.php
+++ routes/web.php
@@ -19,16 +19,17 @@ Auth::routes();
 
 Route::group(['middleware' => 'auth'], function(){
 	Route::get('/home', 'HomeController@index')->name('home');
+	Route::group(['prefix' => 'dashboard', 'middleware' => 'userOnly'], function(){
+		Route::get('/activities', 'ActivityController@index')->name('user.activities');
 
-	Route::get('/activities', 'ActivityController@index')->name('user.activities');
+		Route::get('/payments', 'PaymentController@index')->name('user.payments');
 
-	Route::get('/payments', 'PaymentController@index')->name('user.payments');
+		Route::get('/trustway-investment', 'TrustwayInvestmentController@index')->name('user.investments');
+		Route::get('/trustway-investment/create', 'TrustwayInvestmentController@createForm')->name('user.create-trustway-investments');
+		Route::post('/trustway-investment/create', 'TrustwayInvestmentController@store')->name('user.create-trustway-investments.store');
 
-	Route::get('/trustway-investment', 'TrustwayInvestmentController@index')->name('user.investments');
-	Route::get('/trustway-investment/create', 'TrustwayInvestmentController@createForm')->name('user.create-trustway-investments');
-	Route::post('/trustway-investment/create', 'TrustwayInvestmentController@store')->name('user.create-trustway-investments.store');
-
-	Route::get('/withdrawals', 'WithdrawalController@index')->name('user.withdrawals');
-	Route::post('/withdrawals', 'WithdrawalController@create')->name('user.withdrawals.create');
-	Route::get('/withdrawals/delete/{id}', 'WithdrawalController@delete')->name('user.withdrawals.delete');
+		Route::get('/withdrawals', 'WithdrawalController@index')->name('user.withdrawals');
+		Route::post('/withdrawals', 'WithdrawalController@create')->name('user.withdrawals.create');
+		Route::get('/withdrawals/delete/{id}', 'WithdrawalController@delete')->name('user.withdrawals.delete');
+	});
 });
