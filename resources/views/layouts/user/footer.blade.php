7cb7122ddfaacadad5b13ae9fdb2cef28d41bfba
Jonathan Utsu Undelikwo <jundelikwo@gmail.com>
2019-05-23 18:16:19 +0100

    Changed 'Leadway' to 'Trustway' in user footer

--- resources/views/layouts/user/footer.blade.php
+++ resources/views/layouts/user/footer.blade.php
@@ -1,4 +1,4 @@
 <footer id="footer">
- <p class="pad-lft">&#0169; {{ strftime('%Y', time()) }} Leadway Invest</p>
+ <p class="pad-lft">&#0169; {{ strftime('%Y', time()) }} Trustway Invest</p>
 </footer>
 <button id="scroll-top" class="btn"><i class="fa fa-chevron-up"></i></button>
